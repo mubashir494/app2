a160c8d90e1b30cbecf5cdc8721b4cb31e2b3c8b
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-12-20 19:03:58 -0500

    fixed repi

--- backend/services/remoteServer.js
+++ backend/services/remoteServer.js
@@ -1,44 +1,23 @@
 export const searchCustomer = async (db, query) => {
   let str = queryBuilder(query);
   try {
-    let customer = await db.query(
-      `Select * from dbo.customer as customer where Upper(customer.addr1) like Upper('${str}') OR Upper(customer.addr2) like Upper('%${str}%')`
-    );
-    for (const element of customer.recordset) {
-      const fttx_data = db.query(
-        `Select * from dbo.Contact_fttx_data as data where data.d_custid='${element.id}'`
-      );
-      const dsl_data = db.query(
-        `Select * from dbo.Contact_dsl_data as data where data.d_custid='${element.id}'`
-      );
-      const etherlink_data = db.query(
-        `Select * from dbo.Contact_etherlink_data as data where data.d_custid='${element.id}'`
-      );
-      const wireless_data = db.query(
-        `Select * from dbo.Contact_wireless_data as data where data.d_custid='${element.id}'`
-      );
+    const customerData = await db.query(
+      `select c.id,c.name,c.guarantor,address_type.name,a.addr1,a.addr2,a.city,a.state,a.zip,a.country,
+      svc1.crid,svc2.crid,svc3.crid
+    from customer c
+      inner join address a on c.id=a.idnum and a.idtype='customer'
+      inner join address_type on a.type=address_type.id
+      left outer join Contact_wireless_data svc1 on c.id=svc1.d_custid
+      left outer join contact_fttx_data svc2 on c.id=svc2.d_custid
+      left outer join contact_dsl_data svc3 on c.id=svc3.d_custid
+    where a.addr1 like '%'+${query}+'%' OR a.addr2 like '%'+${query}+'%'
+      OR a.city like '%'+${query}+'%' OR a.state like '%'+${query}+'%'
+      OR a.zip like '%'+${query}+'%' OR a.country like '%'+${query}+'%'
+    order by c.id`)
 
-      const promise = await Promise.all([
-        fttx_data,
-        dsl_data,
-        etherlink_data,
-        wireless_data,
-      ]);
-
-      if (promise[0] && promise[0].recordset) {
-        element.fttx_data = promise[0].recordset;
-      }
-      if (promise[1] && promise[1].recordset) {
-        element.dsl_data = promise[1].recordset;
-      }
-      if (promise[2] && promise[2].recordset) {
-        element.etherlink_data = promise[2].recordset;
-      }
-      if (promise[3] && promise[3].recordset) {
-        element.wireless_data = promise[3].recordset;
-      }
-    }
-    return customer.recordset;
+    
+    
+    return customerData;
   } catch (e) {
     console.log(e.message);
   }
